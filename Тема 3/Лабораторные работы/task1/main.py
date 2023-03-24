7afb756cb51db7639984b32e9fc07e4617d2abf4
AMax <maximenko.alexander96@gmail.com>
2023-03-24 21:49:44 +0300

    lab 2

--- Тема 3/Лабораторные работы/task1/main.py
+++ Тема 3/Лабораторные работы/task1/main.py
@@ -1,7 +1,10 @@
 src = not False and True or False and not True
 
 # TODO расписать упрощение выражения
+# result = True and True or False and False
+# result = True or False
+# result = True
 
-result = ...  # TODO подставить результат выражения
+result = True  # TODO подставить результат выражения
 
 print(src == result)
