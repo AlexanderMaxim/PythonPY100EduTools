69ae87f1f95fa655838a922a9db9b4b7b7bc89e1
AMax <maximenko.alexander96@gmail.com>
2023-04-06 20:40:57 +0300

    lab 3 + lab 4

--- Тема 5/Лабораторная работа/task3/main.py
+++ Тема 5/Лабораторная работа/task3/main.py
@@ -1,5 +1,18 @@
-def get_unique_list_numbers() -> list[int]:
-    ...  # TODO написать функцию для получения списка уникальных целых чисел
+from random import randint  # я почитал, но так и не понял, насколько правильно поместить импорт внутри функции?
+
+
+def get_unique_list_numbers() -> list[int]:  # TODO написать функцию для получения списка уникальных целых чисел
+
+    count = 0
+    list_ = []
+
+    while count <= 15:
+        num = randint(-10, 10)
+        if num not in list_:
+            list_.append(num)
+            count += 1
+
+    return list_
 
 
 list_unique_numbers = get_unique_list_numbers()
--- Тема 5/Лабораторная работа/task5/main.py
+++ Тема 5/Лабораторная работа/task5/main.py
@@ -1,5 +1,11 @@
-def get_random_password() -> str:
-    ...  # TODO написать функцию генерации случайных паролей
+import string
+from random import sample
+
+def get_random_password(n: int = 8) -> str:  # TODO написать функцию генерации случайных паролей
+    chars = string.ascii_letters + string.digits
+    password = "".join(sample(chars, n))
+
+    return password
 
 
 print(get_random_password())
